1bfe8129a4800f6bec6a1b7efbd47e75e0eb1263
Sear Naderi <rsearnaderi@gmail.com>
2024-04-29 18:13:40 +1000

    DB Connection updated

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,3 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-/* Database Connection Set up */
-}
+    private static Connection instance = null;
+    private DatabaseConnection() {
+        String url = "jdbc:sqlite:database.db";
+        try {
+            instance = DriverManager.getConnection(url);
+            } catch (SQLException sqlEx) {
+            System.err.println(sqlEx);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+        }
+        }
+        public static Connection getInstance() {
+            if(instance == null) {
+                new DatabaseConnection();
+            }
+            return instance;
+        }
+    }
+
