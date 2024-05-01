131a6fe50445a3d2715271a012b92671e2c9ff04
Sear Naderi <rsearnaderi@gmail.com>
2024-05-01 19:05:24 +1000

    Added org.xerial:sqlite-jdbc:3.36.0.3 dependency.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,8 @@ module com.example.javafxreadingdemo {
     requires com.almasb.fxgl.all;
     requires java.desktop;
     requires java.sql;
+    requires org.junit.jupiter.api;
+    requires testng;
 
     opens com.example.javafxreadingdemo to javafx.fxml;
     exports com.example.javafxreadingdemo;
