db759ad7f62002819bfa34d94557d12278590b1b
kiri9012 <kkebede00@gmail.com>
2024-05-01 13:51:40 +1000

    Updating project structure, previously sign up and login page were not working as intended. Module-info classes added and project restructuring done to fix this

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.javafxreadingdemo {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens com.example.javafxreadingdemo to javafx.fxml;
     exports com.example.javafxreadingdemo;
